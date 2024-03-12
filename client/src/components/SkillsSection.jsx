dc97bcdcadae338507e954369959dc0425d66a19
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-03-12 22:14:06 +0100

    Add SkillsSection

--- client/src/components/SkillsSection.jsx
+++ client/src/components/SkillsSection.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { skillsData } from "../data/database";
 
 const SkillsSection = () => {
-  return <div>SkillsSection</div>;
+  return (
+    <section id="skills" className="w-full p-2 lg:h-screen">
+      <div className="max-w-[1240px] m-auto flex flex-col justify-center h-full">
+        <p className="uppercase text-xl tracking-widest text-[#5651e5]">Skills</p>
+        <h2 id="skills" className="py-4">
+          What I Can Do
+        </h2>
+        <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-4">
+          {skillsData.map(({ text, img }, index) => {
+            return (
+              <div
+                key={"skills-logo_" + index}
+                className="p-6 duration-300 ease-in shadow-xl rounded-xl hover:scale-105">
+                <div className="grid items-center justify-center grid-cols-2 gap-4">
+                  <div className="m-auto">
+                    <img className="w-[64px] h-[64px]" src={img} alt={text} />
+                  </div>
+                  <div className="flex flex-col items-center justify-center">
+                    <h3 className="uppercase">{text}</h3>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SkillsSection;
