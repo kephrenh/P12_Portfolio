eab9f02205dd6c82c3a6602f795c748df946fb24
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-03-12 21:30:56 +0100

    Database file moved to src folder

---
+++ client/src/data/database.js
@@ -0,0 +1,57 @@
+export const linksData = [
+  {
+    text: "Home",
+    path: "#home",
+  },
+  {
+    text: "About",
+    path: "#about",
+  },
+  {
+    text: "Skills",
+    path: "#skills",
+  },
+  {
+    text: "Works",
+    path: "#works",
+  },
+  {
+    text: "Contact",
+    path: "#contact",
+  },
+];
+
+export const skillsData = [
+  {
+    text: "HTML",
+    img: "/assets/skills/html.png",
+  },
+  {
+    text: "CSS",
+    img: "/assets/skills/css.png",
+  },
+  {
+    text: "JavaScript",
+    img: "/assets/skills/javascript.png",
+  },
+  {
+    text: "Sass",
+    img: "/assets/skills/sass.png",
+  },
+  {
+    text: "React.js",
+    img: "/assets/skills/react.png",
+  },
+  {
+    text: "Tailwind css",
+    img: "/assets/skills/tailwind.png",
+  },
+  {
+    text: "Next.JS",
+    img: "/assets/skills/nextjs.png",
+  },
+  {
+    text: "Github",
+    img: "/assets/skills/github1.png",
+  },
+];
