499f69e3bd681498d12e82e542133cc667edf653
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-03-13 16:33:57 +0100

    Fix SocialIcons component

--- client/src/components/SocialIcons.jsx
+++ client/src/components/SocialIcons.jsx
@@ -7,25 +7,25 @@ import { Link } from "react-router-dom";
 const SocialIcons = ({ padding }) => {
   return (
     <>
-      <Link href="https://www.linkedin.com/in/mohamed-amoussa-665882212/">
+      <Link to="https://www.linkedin.com/in/mohamed-amoussa-665882212/">
         <div
           className={`duration-300 ease-in rounded-full shadow-lg cursor-pointer shadow-gray-400 hover:scale-105 ${padding}`}>
           <FaLinkedinIn />
         </div>
       </Link>
-      <Link href="https://github.com/kephrenh">
+      <Link to="https://github.com/kephrenh">
         <div
           className={`duration-300 ease-in rounded-full shadow-lg cursor-pointer shadow-gray-400 hover:scale-105 ${padding}`}>
           <FaGithub />
         </div>
       </Link>
-      <Link href="mailto: mohamedj81@hotmail.com">
+      <Link to="mailto: mohamedj81@hotmail.com">
         <div
           className={`duration-300 ease-in rounded-full shadow-lg cursor-pointer shadow-gray-400 hover:scale-105 ${padding}`}>
           <AiOutlineMail />
         </div>
       </Link>
-      <Link href="/" title="Call">
+      <Link to="/" title="Call">
         <div
           className={`duration-300 ease-in rounded-full shadow-lg cursor-pointer shadow-gray-400 hover:scale-105 ${padding}`}>
           <BsFillPersonLinesFill />
