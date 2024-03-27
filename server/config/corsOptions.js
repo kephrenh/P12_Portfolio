d03c81ed51efa2366d0401cb4a45609939a10f19
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-03-27 12:25:36 +0100

    fix: cors issues

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,11 +1,10 @@
-const RENDER_URL = process.env.RENDER_URL;
-const FRONTEND = process.env.FRONTEND;
-const FIREBASE_APP = process.env.FIREBASE_APP;
-const MY_WEBSITE = process.env.MY_WEBSITE;
-const LIVE_SERVER = process.env.LIVE_SERVER;
-const LOCAL_SERVER = process.env.LOCAL_SERVER;
-
-const whitelist = [RENDER_URL, FRONTEND, FIREBASE_APP, MY_WEBSITE, LIVE_SERVER, LOCAL_SERVER];
+const whitelist = [
+  "https://p12-portfolio.onrender.com",
+  "https://mamoussa-portfolio.web.app",
+  "https://p12-portfolio.onrender.com",
+  "https://mohamed-amoussa-dev.fr",
+  "http://localhost:5173",
+];
 
 const corsOptionsDelegate = function (req, callback) {
   let corsOptions;
